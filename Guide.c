bb5672e34bffe99639ac3bb1b48fc8fb3aa62e34
nemo1999 <tiny.nemo.fish@gmail.com>
2020-10-18 01:03:48 +0800

    print errors or informations for shaders and programs

--- Guide.c
+++ Guide.c
@@ -15,6 +15,11 @@ bool restart_gl_log ();
 bool gl_log_err (const char* message, ...);
 void glfw_error_callback (int error, const char* description);
 void log_gl_params ();
+void _print_shader_info_log (GLuint shader_index);
+void _print_programme_info_log (GLuint programme);
+void print_all (GLuint programme);
+const char* GL_type_to_string (GLenum type);  
+bool is_valid (GLuint programme);
 
 // reported window size may be good to know for a few things
 int g_win_width = 640;
@@ -175,7 +180,19 @@ int main () {
   glAttachShader (shader_programme, fs);
   glAttachShader (shader_programme, vs);
   glLinkProgram (shader_programme);
+  // check if link was successful
+  params = -1;
+  glGetProgramiv (shader_programme, GL_LINK_STATUS, &params);
+  if (GL_TRUE != params) {
+    fprintf (stderr, "ERROR: could not link shader programme GL index %u\n",shader_programme);
+    _print_programme_info_log (shader_programme);
+    return false;
+  }
+  //validate shaders
+  is_valid(shader_programme);  
 
+
+ 
   glClearColor(0.6f,0.6f,0.8f,1.0f);
   while (!glfwWindowShouldClose (window)) {
     _update_fps_counter(window);
@@ -313,17 +330,112 @@ void log_gl_params () {
 
 /* we will use this function to update the window title with a frame rate */
 void _update_fps_counter (GLFWwindow* window) {
-double current_seconds;
-double elapsed_seconds;
-current_seconds = glfwGetTime ();
-elapsed_seconds = current_seconds - previous_seconds;
-/* limit text updates to 4 per second */
-if (elapsed_seconds > 0.25) {
-previous_seconds = current_seconds;
-char tmp[128];
-double fps = (double)frame_count / elapsed_seconds;
-sprintf (tmp, "opengl @ fps: %.2f", fps);glfwSetWindowTitle (window, tmp);
-frame_count = 0;
+  double current_seconds;
+  double elapsed_seconds;
+  current_seconds = glfwGetTime ();
+  elapsed_seconds = current_seconds - previous_seconds;
+  /* limit text updates to 4 per second */
+  if (elapsed_seconds > 0.25) {
+    previous_seconds = current_seconds;
+    char tmp[128];
+    double fps = (double)frame_count / elapsed_seconds;
+    sprintf (tmp, "opengl @ fps: %.2f", fps);glfwSetWindowTitle (window, tmp);
+    frame_count = 0;
+  }
+  frame_count++;
 }
-frame_count++;
+void _print_shader_info_log (GLuint shader_index) {
+  int max_length = 2048;
+  int actual_length = 0;
+  char log[2048];
+  glGetShaderInfoLog (shader_index, max_length, &actual_length, log);
+  printf ("shader info log for GL index %u:\n%s\n", shader_index, log);
+}
+void _print_programme_info_log (GLuint programme) {
+  int max_length = 2048;
+  int actual_length = 0;
+  char log[2048];
+  glGetProgramInfoLog (programme, max_length, &actual_length, log);
+  printf ("program info log for GL index %u:\n%s", programme, log);
+}
+void print_all (GLuint programme) {
+  printf ("--------------------\nshader programme %i info:\n", programme);
+  int params = -1;
+  glGetProgramiv (programme, GL_LINK_STATUS, &params);
+  printf ("GL_LINK_STATUS = %i\n", params);
+  glGetProgramiv (programme, GL_ATTACHED_SHADERS, &params);
+  printf ("GL_ATTACHED_SHADERS = %i\n", params);
+  glGetProgramiv (programme, GL_ACTIVE_ATTRIBUTES, &params);
+  printf ("GL_ACTIVE_ATTRIBUTES = %i\n", params);
+  for (GLuint i = 0; i < (GLuint)params; i++) {
+    char name[64];
+    int max_length = 64;
+    int actual_length = 0;
+    int size = 0;
+    GLenum type;
+    glGetActiveAttrib (programme, i, max_length, &actual_length, &size, &type,name);
+    if (size > 1) {
+      for (int j = 0; j < size; j++) {
+	char long_name[64];
+	sprintf (long_name, "%s[%i]", name, j);
+	int location = glGetAttribLocation (programme, long_name);
+	printf (" %i) type:%s name:%s location:%i\n", i, GL_type_to_string (type),long_name, location);
+      }
+    } else {
+      int location = glGetAttribLocation (programme, name);
+      printf (" %i) type:%s name:%s location:%i\n", i, GL_type_to_string (type),name, location);
+    }
+  }
+  glGetProgramiv (programme, GL_ACTIVE_UNIFORMS, &params);
+  printf ("GL_ACTIVE_UNIFORMS = %i\n", params);
+  for (GLuint i = 0; i < (GLuint)params; i++) {
+    char name[64];
+    int max_length = 64;
+    int actual_length = 0;
+    int size = 0;
+    GLenum type;
+    glGetActiveUniform (programme, i, max_length, &actual_length, &size, &type,name);
+    if (size > 1) {
+      for (int j = 0; j < size; j++) {
+	char long_name[64];
+	sprintf (long_name, "%s[%i]", name, j);
+	int location = glGetUniformLocation (programme, long_name);
+	printf (" %i) type:%s name:%s location:%i\n", i, GL_type_to_string (type),long_name, location);
+      }
+    } else {
+      int location = glGetUniformLocation (programme, name);
+      printf (" %i) type:%s name:%s location:%i\n", i, GL_type_to_string (type),name, location);
+    }
+  }
+  _print_programme_info_log (programme);
+}
+const char* GL_type_to_string (GLenum type) {
+  switch (type) {case GL_BOOL: return "bool";
+  case GL_INT: return "int";
+  case GL_FLOAT: return "float";
+  case GL_FLOAT_VEC2: return "vec2";
+  case GL_FLOAT_VEC3: return "vec3";
+  case GL_FLOAT_VEC4: return "vec4";
+  case GL_FLOAT_MAT2: return "mat2";
+  case GL_FLOAT_MAT3: return "mat3";
+  case GL_FLOAT_MAT4: return "mat4";
+  case GL_SAMPLER_2D: return "sampler2D";
+  case GL_SAMPLER_3D: return "sampler3D";
+  case GL_SAMPLER_CUBE: return "samplerCube";
+  case GL_SAMPLER_2D_SHADOW: return "sampler2DShadow";
+  default: break;
+  }
+  return "other";
+}
+
+bool is_valid (GLuint programme) {
+    glValidateProgram (programme);
+    int params = -1;
+    glGetProgramiv (programme, GL_VALIDATE_STATUS, &params);
+    printf ("program %i GL_VALIDATE_STATUS = %i\n", programme, params);
+    if (GL_TRUE != params) {
+      _print_programme_info_log (programme);
+      return false;
+    }
+    return true;
 }
